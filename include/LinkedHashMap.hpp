5144d9687d5b18fb9a077ab6ed7fcc1d0bfd10cf
jbouhier <jb.bouhier@gmail.com>
2016-10-07 09:50:08 +0200

    Added .hpp for latest changes

--- include/LinkedHashMap.hpp
+++ include/LinkedHashMap.hpp
@@ -3,7 +3,7 @@
 
 #include <iostream>
 
-#define HASHSIZE 50000
+#define HASHSIZE 100000
 
 
 typedef struct		s_data
@@ -35,6 +35,7 @@ class LinkedHashMap
 		t_data			*create_new_node_with_key_and_value(const char *key, const char *value);
 		t_data			*update_node_with_key_for_value(const char *key,const char *value);
 		t_data			*install_new_node_in_hashtab(t_data *node, t_data *hashtab[HASHSIZE]);
+		int				my_getnbr(char *str);
 };
 
 #endif /* !LINKEDHASHMAP_HPP */
--- include/utilities.hpp
+++ include/utilities.hpp
@@ -8,4 +8,7 @@ void	debug_add_cmd(LinkedHashMap **all_datas, char *key, char *value);
 void	debug_delete_cmd(LinkedHashMap **all_datas, char *key);
 void	debug_init(LinkedHashMap **all_datas, char **cmd_val, int nb_words);
 
+
+using namespace std;
+
 #endif /* !UTILITIES_HPP */
